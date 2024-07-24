cb7bed2d361e12ae591d8f81ae48812ac69b274a
Abbas-Davodkhani <davodkhaniabbas2003@gmail.com>
2024-07-24 16:54:48 +0330

    gemini clone

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../src/Components/Sidebar/Sidebar"
+
+const app = () => {
+  return (
+    <>
+      <Sidebar/>
+    </>
+  )
+}
+
+export default app;
