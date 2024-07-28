390eccc486d34e2fee5c376da7154f0ec2f8b614
Abbas-Davodkhani <davodkhaniabbas2003@gmail.com>
2024-07-28 15:19:16 +0330

    completed v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Sidebar from "../src/Components/Sidebar/Sidebar"
+import Main from "../src/Components/Main/Main";
+
 
 const app = () => {
   return (
     <>
       <Sidebar/>
+      <Main />
     </>
   )
 }
